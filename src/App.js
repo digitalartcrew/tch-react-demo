7a5fbb26f25ee7eb59a4585376b356b1595f5053
Nicholas Burney <nicholasburney@Nickbook.local>
2021-10-03 11:42:42 -1000

    setup basic routing

--- src/App.js
+++ src/App.js
@@ -1,83 +1,107 @@
 import "./App.css";
 import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
-	const recipes = [
-		{
-			id: 1,
-			imageSrc: "https://via.placeholder.com/200x200",
-			title: "Title1",
-			description:
-				"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, vero quisquam magnam voluptatibus minus asperiores excepturi distinctio voluptas! Sint at facilis alias suscipit aut quia ea in sequi quisquam nulla!",
-		},
-		{
-			id: 2,
-			imageSrc: "https://via.placeholder.com/200x200",
-			title: "Title2",
-			description:
-				"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, vero quisquam magnam voluptatibus minus asperiores excepturi distinctio voluptas! Sint at facilis alias suscipit aut quia ea in sequi quisquam nulla!",
-		},
-		{
-			id: 3,
-			imageSrc: "https://via.placeholder.com/200x200",
-			title: "Title3",
-			description:
-				"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, vero quisquam magnam voluptatibus minus asperiores excepturi distinctio voluptas! Sint at facilis alias suscipit aut quia ea in sequi quisquam nulla!",
-		},
-		{
-			id: 4,
-			imageSrc: "https://via.placeholder.com/200x200",
-			title: "Title4",
-			description:
-				"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, vero quisquam magnam voluptatibus minus asperiores excepturi distinctio voluptas! Sint at facilis alias suscipit aut quia ea in sequi quisquam nulla!",
-		},
-	];
+  const recipes = [
+    {
+      id: 1,
+      imageSrc: "https://via.placeholder.com/200x200",
+      title: "Title1",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, vero quisquam magnam voluptatibus minus asperiores excepturi distinctio voluptas! Sint at facilis alias suscipit aut quia ea in sequi quisquam nulla!",
+    },
+    {
+      id: 2,
+      imageSrc: "https://via.placeholder.com/200x200",
+      title: "Title2",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, vero quisquam magnam voluptatibus minus asperiores excepturi distinctio voluptas! Sint at facilis alias suscipit aut quia ea in sequi quisquam nulla!",
+    },
+    {
+      id: 3,
+      imageSrc: "https://via.placeholder.com/200x200",
+      title: "Title3",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, vero quisquam magnam voluptatibus minus asperiores excepturi distinctio voluptas! Sint at facilis alias suscipit aut quia ea in sequi quisquam nulla!",
+    },
+    {
+      id: 4,
+      imageSrc: "https://via.placeholder.com/200x200",
+      title: "Title4",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, vero quisquam magnam voluptatibus minus asperiores excepturi distinctio voluptas! Sint at facilis alias suscipit aut quia ea in sequi quisquam nulla!",
+    },
+  ];
 
-	const [recipeCollection, setRecipeCollection] = useState([...recipes]);
-	return (
-		<div className="App">
-			<HomePage collection={recipeCollection} />
+  const [recipeCollection, setRecipeCollection] = useState([...recipes]);
+
+  return (
+      <Router>
+        <div className="App">
+			<NavBar />
+			<Switch>
+				<Route path="/add-recipe">
+					<AddRecipePage />
+				</Route>
+				<Route path="/">
+					<HomePage collection={recipeCollection} />
+				</Route>
+			</Switch>
 		</div>
-	);
+	</Router>
+  );
 };
 
 const HomePage = ({ collection }) => {
-	return (
-		<div className="home-page-container">
-			<NavBar />
-			<div className="card-container">
-				{collection.map((recipe) => (
-					<EntityCard entity={recipe} />
-				))}
-			</div>
-		</div>
-	);
+  return (
+      <div className="home-page-container">
+        <div className="card-container">
+          {collection.map((recipe) => <EntityCard entity={recipe} key={recipe.id} />)}
+        </div>
+      </div>
+  );
 };
 
 const NavBar = () => {
-	return (
-		<ul className="navigation-bar">
-			<li>Tech, Chicken & HS</li>
-			<li>
-				<input type="text" placeholder="Search..." />
-			</li>
-			<li>
-				<button onClick="null">Add +</button>
-			</li>
-		</ul>
-	);
+  return (
+      <div>
+        <nav>
+          <ul className="navigation-bar">
+            <li>
+              <Link to="/">Tech, Chicken and HS</Link>
+            </li>
+            <li>
+              <input type="text" placeholder="Search..." />
+            </li>
+            <li>
+              <Link to="/add-recipe">Add +</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+  );
 };
 
 const EntityCard = ({ entity }) => {
+  return (
+    <div className="entity-card">
+      <img src={entity.imageSrc} alt="placeholder card" />
+      <div className="entity-card-details">
+        <h5>{entity.title}</h5>
+        <p className="entity-card-description">{entity.description}</p>
+      </div>
+    </div>
+  );
+};
+
+const AddRecipePage = () => {
 	return (
-		<div className="entity-card">
-			<img src={entity.imageSrc} alt="placeholder card" />
-			<div className="entity-card-details">
-				<h5>{entity.title}</h5>
-				<p className="entity-card-description">{entity.description}</p>
-			</div>
+		<div>
+			<h1>Add Recipe Page</h1>
+			<form></form>
 		</div>
-	);
+	)
 };
 
 export default App;
