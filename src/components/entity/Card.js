771c5111a5dea40491c827f23aa6c2f4debb76d4
digitalartcrew <digitalartcrew@gmail.com>
2021-10-04 14:52:45 -1000

    lined em up

--- src/components/entity/Card.js
+++ src/components/entity/Card.js
@@ -3,7 +3,7 @@ import Button from "react-bootstrap/Button";
 
 const EntityCard = ({ entity }) => {
 	return (
-		<Card style={{ width: "18rem" }}>
+		<Card className="col-3">
 			<Card.Img variant="top" src={entity.imageSrc} />
 			<Card.Body>
 				<Card.Title>{entity.title}</Card.Title>
